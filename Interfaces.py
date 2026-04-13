6d1d1a5b815c664ff31158b330226e40c940a4a9
MaxLys <maksimus3208@gmail.com>
2026-04-13 20:18:37 +0300

    Refactor IUserRepository and update IDishRepository

--- Interfaces.py
+++ Interfaces.py
@@ -1,10 +1,27 @@
 from abc import ABC, abstractmethod
-from typing import List, Optional
-from models import User, Dish
+from typing import Optional, List
+from Domain.Models.models import User, HistoryRecord
+from Domain.Models.recipe import Dish
 
 class IUserRepository(ABC):
     @abstractmethod
-    def get_by_username(self, username: str) -> Optional[User]:
+    def get_by_username(self, username: str) -> Optional[User]: 
+        pass
+
+    @abstractmethod
+    def create(self, user: User) -> User: 
+        pass
+
+    @abstractmethod
+    def delete(self, user_id: int) -> bool: 
+        pass
+
+    @abstractmethod
+    def add_to_history(self, user_id: int, recipe_id: int) -> None: 
+        pass
+
+    @abstractmethod
+    def get_history(self, user_id: int) -> List[HistoryRecord]: 
         pass
 
 class IDishRepository(ABC):
